3d8b2f6605bc327c43fa5935889770790bfaf2c5
Leon Yu <leon@leonyu.net>
2023-09-21 01:24:36 -0700

    Fix build.sh for Docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-git clean -dfx
+if command -v git; then
+    git clean -dfx
+fi
 mkdir -p dist
 python3 -m zipapp -c mysql_visual_explain_cli -o ./dist/mysql_visual_explain_cli.pyz
 7z l ./dist/mysql_visual_explain_cli.pyz
