c00655505fbf9b0ecc04dfd63722de385598591b
Leon Yu <leon@leonyu.net>
2023-09-21 02:39:06 -0700

    Fix version name

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 source "$SCRIPT_DIR/build.sh"
 source "$SCRIPT_DIR/test.sh"
-gh release create --generate-notes "$(date +%Y.%m.%d)" ./dist/mysql_visual_explain_cli.pyz
+gh release create --generate-notes "v$(date +%Y.%m.%d)" ./dist/mysql_visual_explain_cli.pyz
