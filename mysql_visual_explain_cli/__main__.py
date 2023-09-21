42408ba8a2db3444e4e4b537c311e5f2c384f6b6
Leon Yu <leon@leonyu.net>
2023-09-21 01:05:18 -0700

    autopep8 __main__.py

--- mysql_visual_explain_cli/__main__.py
+++ mysql_visual_explain_cli/__main__.py
@@ -1,23 +1,29 @@
+
 import argparse
 
-from query_analysis import explain_renderer
+from query_analysis.explain_renderer import ExplainContext, decode_json
 
 
-def render(json_data, output_path):
-    ctx = explain_renderer.ExplainContext(explain_renderer.decode_json(json_data), None)
+def render(json_data: str, output_path: str):
+    ctx = ExplainContext(decode_json(json_data), None)
     ctx.init_canvas(None, None, lambda x, y, w, h: None)
     ctx.layout()
     ctx.export_to_png(output_path)
 
+
 def main():
-    parser = argparse.ArgumentParser(description="CLI to convert MySQL JSON EXPLAIN to PNG file.")
-    parser.add_argument('infile', type=argparse.FileType('r'), help="Input file (must be JSON)")
-    parser.add_argument('outfile', help="Output file path (output is PNG)")
+    parser = argparse.ArgumentParser(
+        description='Convert MySQL FORMAT=JSON EXPLAIN to PNG file.')
+    parser.add_argument('infile',
+                        type=argparse.FileType('r'),
+                        help='Input JSON file (Use - for stdin)')
+    parser.add_argument('outfile', help='Output PNG file path')
     args = parser.parse_args()
 
-    with args.infile as f:
-        json_data = f.read().replace('\n','')
+    with args.infile as file:
+        json_data = file.read().replace('\n', '')
         render(json_data, args.outfile)
+
 
 if __name__ == '__main__':
     main()
