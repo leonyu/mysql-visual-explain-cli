212b32e162ffd00885f0b5349fb79635f3cf82ec
Leon Yu <leon@leonyu.net>
2023-09-30 10:12:03 -0700

    Make build.sh work without git

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-set -e
-
-if command -v git; then
-    git clean -dfx
-fi
+rm -rf ./dist/mysql_visual_explain_cli.pyz
 mkdir -p dist
 python3 -m zipapp -c mysql_visual_explain_cli -p '/usr/bin/env python3' -o ./dist/mysql_visual_explain_cli.pyz
 unzip -l ./dist/mysql_visual_explain_cli.pyz
