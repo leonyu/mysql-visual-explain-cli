8e9d6fd4b408689837e637ecf21e8de41bf79739
Leon Yu <leon@leonyu.net>
2024-08-06 02:08:14 -0400

    Improve SVG testing

--- test/reference_test.py
+++ test/reference_test.py
@@ -1,37 +1,37 @@
-import os
-import subprocess
-import tempfile
 import glob
+import itertools
+import os
+import pathlib
+import re
+import subprocess
 import sys
-from pathlib import Path
+import tempfile
 
-from PIL import Image, ImageChops
-import numpy as np
+import cairosvg
+import PIL
 import pytest
-from cairosvg import svg2png
 
 
 def compare_png(png1: str, png2: str) -> float:
-    image1 = Image.open(png1)
-    image2 = Image.open(png2)
-    diff = ImageChops.difference(image1, image2)
-    (width1, height1) = image1.size
-    (width2, height2) = image2.size
-    return (
-        2
-        * np.asarray(diff, dtype="int32").sum()
-        / (256 * (width1 * height1 + width2 * height2))
-    )
+    image1 = PIL.Image.open(png1)
+    image2 = PIL.Image.open(png2)
+    diff = PIL.ImageChops.difference(image1, image2)
+    (width, height) = diff.size
+    return sum(itertools.chain.from_iterable(diff.getdata())) / (256 * (width * height))
+
+
+def remove_pt(svg_content: str) -> str:
+    return re.sub(r'"(\d+)pt"', r'"\1"', svg_content)
 
 
 def compare_svg(svg1: str, svg2: str) -> float:
-    svg1_content = Path(svg1).read_text()
-    svg2_content = Path(svg2).read_text()
+    svg1_content = pathlib.Path(svg1).read_text()
+    svg2_content = pathlib.Path(svg2).read_text()
     with tempfile.TemporaryDirectory() as tmp:
         png1 = f"{tmp}/svg1.png"
         png2 = f"{tmp}/svg2.png"
-        svg2png(svg1_content, write_to=png1)
-        svg2png(svg2_content, write_to=png2)
+        cairosvg.svg2png(remove_pt(svg1_content), write_to=png1)
+        cairosvg.svg2png(remove_pt(svg2_content), write_to=png2)
         return compare_png(png1, png2)
 
 
@@ -41,7 +41,7 @@ class TestClass:
     output_dir = "/tmp"
 
     def make_file(self, json_file: str, output_ext: str) -> tuple[str, str]:
-        json_path = Path(json_file)
+        json_path = pathlib.Path(json_file)
         ref_file = os.path.join("reference", f"{json_path.stem}.{output_ext}")
         assert os.path.exists(ref_file)
         output_file = os.path.join(self.output_dir, f"{json_path.stem}.{output_ext}")
@@ -63,4 +63,4 @@ class TestClass:
     def test_svg_image(self, json_file: str):
         (output_file, ref_file) = self.make_file(json_file, "svg")
         diff = compare_svg(output_file, ref_file)
-        assert diff < 0.65  # We need better SVG comparison
+        assert diff < 0.1
