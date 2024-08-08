e56ed7ce4dda6f0856912ba752300c6d77c248a2
Leon Yu <leon@leonyu.net>
2024-08-08 02:34:59 -0400

    Fix release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,5 +5,6 @@ set -e
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 "$SCRIPT_DIR"/build.sh
-"$SCRIPT_DIR"/test.sh
+poetry install
+poetry run pytest
 gh release create --generate-notes "v$(date +%Y.%m.%d)" "$SCRIPT_DIR"/../dist/mysql_visual_explain_cli.pyz
